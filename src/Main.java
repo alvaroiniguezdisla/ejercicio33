64eb39ff26eeddf52a0352ba79cb3da776c71f67
Álvaro <ainiguezdislaa@gmail.com>
2023-03-23 14:41:12 +0100

    ej 33 terminado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int n = sc.nextInt();
+        System.out.println("La suma de los números desde 1 hasta " + n + " es: " + ej33.suma(n));
+        System.out.println("La suma de los números desde 1 hasta " + n + " es: " + ej33.suma2(n));
     }
 }
